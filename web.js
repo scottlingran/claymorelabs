5f59c62c032d05f9c25049bef8d9d9759ef457bc
Scott Li <scottli_010@hotmail.com>
2015-06-15 12:41:31 -0700

    save

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ var fs = require('fs')
 var app = express();
 app.use(compression())
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/dist'))
 
 app.use(function(req, res) {
-	res.sendfile(__dirname + '/public/index.html');
+  res.sendfile(__dirname + '/dist/index.html');
 })
 
 app.listen(process.env.PORT || 8000);
